cd203cace8c26e287e0ccb2a53cc2e6af7c13614
Aditya <192105adityashah@gmail.com>
2024-02-27 19:58:05 +0530

    Added new files

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import Home from "./Pages/Home/Home";
 import Forum from "./Pages/Forum/Forum";
 import GrpChat from "./Pages/Communities/Chat";
 import Addevent from "./Components/Forms/Addevent";
+import Multi from "./Components/Multi/Multi";
+
 
 const App = () => {
   return (
@@ -33,6 +35,7 @@ const App = () => {
             <Route path="/home" element={<Home />} />
             <Route path="/forum" element={<Forum />} />
             <Route path="/addevent" element={<Addevent />} />
+            <Route path="/multi" element={<Multi />} />
             {/* PRIVATE ROUTES BELOW THIS */}
             {/* <Route path="/" element={<Private />}>
             <Route path="/editcustomer" element={<CustomerEdiit />} />
---
+++ src/Components/Multi/Multi.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Search from "./Search";
+import Mode from "./Mode";
+
+
+const Multi = () => {
+  return (
+    <div className="ml-52">
+      <Search/>
+        <Mode/>
+    </div>
+  );
+};
+
+export default Multi;
