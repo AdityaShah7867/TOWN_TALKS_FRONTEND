720e5fcbbac8f862a9cdbc9bbbc7caa47e07159c
Aditya <192105adityashah@gmail.com>
2024-02-27 16:35:22 +0530

    added new pages

--- src/Pages/Forum/Forum.jsx
+++ src/Pages/Forum/Forum.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import Post from '../../Components/Forum/Post';
+import React from "react";
+import Post from "../../Components/Forum/Post";
+import PostCard from "../../Components/Forum/PostCard";
 
 const Forum = () => {
   return (
-    <div className='bg-gray-100 min-h-screen '>
-        <br/>
-    <div className="flex justify-center items-center ">
-      <div className="w-1/3">
-        <Post />
+    <div className="bg-gray-100 min-h-screen ">
+      <br />
+      <div className="flex justify-center items-center ">
+        <div className="w-1/3">
+          <Post />
+          <PostCard className='mt-12' />
+        </div>
+        
       </div>
-    </div>
+      
     </div>
   );
 };
